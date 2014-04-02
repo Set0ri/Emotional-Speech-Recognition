077e820b2ff253aa12f3b0de39b9927047e032e5
Jennifer Blight <jgblight@v1020-wn-127-77.campus-dynamic.uwaterloo.ca>
2014-04-01 22:34:00 -0400

    grid search

--- train.m
+++ train.m
@@ -1,24 +1,40 @@
-function train(labels,values)
+function [best_net,best_rate,skill_matrix] = train(labels,values)
   cutoff = 5000;
   K = 10;
   shuffle = randperm(size(labels,1));
   shuffle = shuffle(1:cutoff);
   labels = labels(shuffle,:);
   values = values(shuffle,:);
+  
+  clusters = [2,4,8,16];
+  spread = [0.01,0.1,1,10];
+  
+  skill_matrix = zeros(length(clusters),length(spread));
+  best_rate = 0;
+  
  
-  [center,u,o] = fcm(values,10);
+  for c_num = 1:length(clusters)
+      [center,u,o] = fcm(values,clusters(c_num));
+
+      for spr_num = 1:length(spread)
+          indices = crossvalind('Kfold',cutoff,K);
+          [m,class] = max(labels,[],2);
+          cp = classperf(class);
+          for i = 1:K
+            test = (indices == i); train = ~test;
+            net = newrbe(u(:,train),labels(train,:)',spread(spr_num));
+            y = sim(net,u(:,test));
+            [m,class] = max(y,[],1);
+            classperf(cp,class,test);
+          end
+
+          skill_matrix(c_num,spr_num) = cp.CorrectRate;
+          if cp.CorrectRate > best_rate
+             best_rate = cp.CorrectRate
+             best_net = net;
+          end
+      end
   
-  indices = crossvalind('Kfold',cutoff,K);
-  [m,class] = max(labels,[],2);
-  cp = classperf(class);
-  for i = 1:K
-    test = (indices == i); train = ~test;
-    net = newrbe(u(:,train),labels(train,:)');
-    y = sim(net,u(:,test));
-    [m,class] = max(y,[],1);
-    classperf(cp,class,test);
   end
-  
-  cp.ErrorRate
 
 end
