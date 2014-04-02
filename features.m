a3a9bd72c83c7018e78002b314ca66dc2743150f
Jennifer Blight <jgblight@v1020-wn-127-77.campus-dynamic.uwaterloo.ca>
2014-04-01 20:31:19 -0400

    added fcm

--- features.m
+++ features.m
@@ -1,10 +1,11 @@
-function [labels,values] = features(folder,bl,win)
-  labels = {'a','d','f','h','n','sa','su'};
-  values = {[],[],[],[],[],[],[]};
+function net = features(folder,bl,win)
+  label_set = {'a','d','f','h','n','sa','su'};
+  labels = [];
+  values = [];
   for subject = dir(folder)'
     if subject.isdir && ~(strcmp(subject.name,'.')||strcmp(subject.name,'..'))
-      for l = 1:numel(labels)
-        for file = dir([folder,'/',subject.name,'/',labels{l},'*.wav'])'
+      for l = 1:numel(label_set)
+        for file = dir([folder,'/',subject.name,'/',label_set{l},'*.wav'])'
           [d,sr] = wavread([folder,'/',subject.name,'/',file.name]);
           f = PitchTimeZeroCrossings(d,win*sr,bl*sr,sr);
           p = rastaplp(d,sr,1,12,'wintime',win,'hoptime',bl);
@@ -13,10 +14,17 @@ function [labels,values] = features(folder,bl,win)
           max_len = min([size(f,2),size(p,2),size(m,2)]);
           sample = [f(:,1:max_len); p(:,1:max_len); m(:,1:max_len)];
           sample = sample(:,isfinite(sample(1,:)));
-          values{l} = [values{l}; sample'];
+          values = [values; sample'];
+          labels = [labels l];
+          
         end
       end
     end
   end
+  
+  [center,u,o] = fcm(values,10);
+  
+  net = newrbe(u,labels);
+  
 
 end
